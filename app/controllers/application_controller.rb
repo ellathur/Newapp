0d17e24ed9563383406ac13aac4d0e1b5685e8f5
ellathur <babylonhustle@live.co.uk>
2017-03-20 00:34:57 +0000

    added color field to products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def product_params
+    params.require(:product).permit(:name, :description, :image_url, :color)
+  end
+  
 end
