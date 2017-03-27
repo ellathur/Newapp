e769eafcd72ee25ab32507937907f9081427cfa2
ellathur <babylonhustle@live.co.uk>
2017-03-27 14:56:18 +0100

    Added price place holder to product pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326214057) do
+ActiveRecord::Schema.define(version: 20170327130514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170326214057) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.decimal  "price"
   end
 
 end
