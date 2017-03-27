4185e674d39a48c7101640374bce523ae363bba7
ellathur <babylonhustle@live.co.uk>
2017-03-27 20:28:31 +0100

    updated database to fix duplicate colums

--- db/migrate/20170327130514_add_price_to_products.rb
+++ db/migrate/20170327130514_add_price_to_products.rb
@@ -1,5 +1,7 @@
 class AddPriceToProducts < ActiveRecord::Migration[5.0]
   def change
     add_column :products, :price, :decimal
+    if column_exists? :table_name, :column_name
+               remove_column :table_name, :column_name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20170327130514) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "colour"
     t.decimal  "price"
   end
 
