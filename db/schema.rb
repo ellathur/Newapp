096f8a2194fb85112e81479aef5d85b3d242a04b
ellathur <babylonhustle@live.co.uk>
2017-03-28 01:59:33 +0100

    t.string color from database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327210120) do
+ActiveRecord::Schema.define(version: 20170328005042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20170327210120) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.decimal  "price"
-    t.string   "color"
     t.string   "colour"
   end
 
