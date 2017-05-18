736cd4d695d65cd0824080c713e1639dbe635c8d
ellathur <babylonhustle@live.co.uk>
2017-05-18 16:36:04 +0100

    added stripe to use factory

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key: ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    publishable_key: 'pk_test_2BiTbYp777ybBnASkaQHgpxs',
+    secret_key: 'sk_test_'
+  }
+end
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+
+  sequence(:email) { |n| "user#{n}@yahoo.co.uk" }
+  sequence(:password) { |n| "1234567890#{n}" }
+
+  factory :user do
+    email
+    password #{}"1234567890"
+    first_name "Peter"
+    last_name "Example"
+    admin false
+   end
+end
