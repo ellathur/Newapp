641f2f259222409e59d209cd9f2ac92e4ee9adf0
ellathur <babylonhustle@live.co.uk>
2017-05-20 04:28:10 +0100

    added elevateZoom plugin to app and implemented it in product show pages

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,6 @@
 
 $(document).on('turbolinks:load', function(){
+    $('.img-zoom').elevateZoom();
     $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
           readOnly: true,
