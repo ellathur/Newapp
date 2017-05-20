d54170849f8d10139bbeb7c6808bef823a6db0bf
ellathur <babylonhustle@live.co.uk>
2017-05-20 15:23:42 +0100

    fixed payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,9 +1,10 @@
 class PaymentsController < ApplicationController
 
   def create
+    token = params[:stripeToken]
     @product = Product.find(params[:product_id])
     @user = current_user
-    token = params[:stripeToken]
+
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
@@ -16,6 +17,8 @@ class PaymentsController < ApplicationController
 
     if charge.paid
       Order.create(product_id: @product.id, user_id: @user.id, total: @product.price, created_at: Time.now)
+      UserMailer.payment_confirmation(@user, @product).deliver_now
+      redirect_to product_path(@product), notice: 'Purchase Successful. Thank You!'
     end
 
     rescue Stripe::CardError => e
@@ -25,6 +28,6 @@ class PaymentsController < ApplicationController
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
 
-    redirect_to product_path(@product), notice: 'Purchase Successful. Thank You!'
+
   end
 end
