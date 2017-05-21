07531c62ddaa4330d34148ebf65ab9b2d217c2fb
ellathur <babylonhustle@live.co.uk>
2017-05-21 11:43:02 +0100

    added AJAX to create comments remotely

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,27 @@
-
-$(document).on('turbolinks:load', function(){
-    $('.img-zoom').elevateZoom();
-    $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-          readOnly: true,
-          score: function() {
+        readOnly: true,
+        score: function() {
             return $(this).attr('data-score');
-          }
-        });
+        }
+    });
+};
+
+
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+
+
+     $(".alert").delay(4000).fadeOut("slow");
+     refreshRating();
+
+      /* elevateZoom*/
+     $('.img-zoom').elevateZoom({
+        zoomType: "lens",
+        lensShape: "round",
+        lensSize: 300,
+        lensFadeIn: 500,
+        lensFadeOut: 500
+    });
 });
