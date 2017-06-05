e88b9b8955b8e6d19a6fdce2ed855dc64d7f4569
ellathur <babylonhustle@live.co.uk>
2017-06-05 14:48:08 +0100

    modified database schema to include postgresql

---
+++ db/migrate/20170605134211_finalwork.rb
@@ -0,0 +1,4 @@
+class Finalwork < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508000719) do
+ActiveRecord::Schema.define(version: 20170605134211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
